e5839aaedbc935cd2b599a2ee39f20de1d7dfb6d
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-07-17 23:49:01 +0100

    Add missing tlast signal; Change variable names.

--- hardware/src/iob_axistream_out.v
+++ hardware/src/iob_axistream_out.v
@@ -29,7 +29,8 @@ module iob_axistream_out
 `include "iob_axistream_out_swreg_gen.vh"
    
    `IOB_WIRE(fifo_empty, 1)
-   `IOB_VAR(tvalid_tmp, 1)
+   `IOB_VAR(tvalid_int, 1)
+   `IOB_VAR(tlast_int, 1)
    //FIFO RAM
    `IOB_WIRE(ext_mem_w_en, num_outputs_per_input)
    `IOB_WIRE(ext_mem_w_data, 32)
@@ -41,21 +42,26 @@ module iob_axistream_out
    // Set unused rdata bits to 0
    `IOB_WIRE2WIRE({(`AXISTREAMOUT_FULL_W-1){1'b0}}, AXISTREAMOUT_FULL_rdata[`AXISTREAMOUT_FULL_W-1:1])
 
+	//Register to store tlast wstrb
    `IOB_WIRE(last_wstrb, num_outputs_per_input)
    iob_reg #(.DATA_W(num_outputs_per_input))
    axistreamout_wstrb_next_word_last (
        .clk        (clk),
        .arst       (rst),
        .arst_val   (1'b0),
-	    .rst        (fifo_empty & written_last_word), //Reset when TLAST byte is sent
+	    .rst        (fifo_empty & storing_tlast_word), //Reset when TLAST word is sent
        .rst_val    (1'b0),
        .en         (AXISTREAMOUT_WSTRB_NEXT_WORD_LAST_en), 
        .data_in    (AXISTREAMOUT_WSTRB_NEXT_WORD_LAST_wdata[0+:num_outputs_per_input]),
        .data_out   (last_wstrb) 
    );
 
-   `IOB_VAR(written_last_word, 1)
-   `IOB_REG_RE(clk, fifo_empty & written_last_word, 1'b0, last_wstrb & AXISTREAMOUT_IN_en, written_last_word, 1'b1) //reset when TLAST byte is sent
+   //Signal if tlast word is stored.
+	//Enable when TLAST word wstrb has been defined (in the last_wstrb register) 
+	//and a value has been inserted word.
+	//Reset when TLAST word is sent.
+   `IOB_VAR(storing_tlast_word, 1)
+   `IOB_REG_RE(clk, fifo_empty & storing_tlast_word, 1'b0, last_wstrb & AXISTREAMOUT_IN_en, storing_tlast_word, 1'b1) 
   
    `IOB_WIRE(fifo_level, FIFO_DEPTH_LOG2+1)
    iob_fifo_sync
@@ -86,19 +92,32 @@ module iob_axistream_out
       .level           (fifo_level)
       );
   
-   `IOB_WIRE2WIRE(fifo_full | |last_wstrb, AXISTREAMOUT_FULL_rdata[0]) //Set FIFO full register when it is full or is waiting to send TLAST byte.
+   //Set FIFO full register when it is full or is waiting to send TLAST word.
+   `IOB_WIRE2WIRE(fifo_full | |last_wstrb, AXISTREAMOUT_FULL_rdata[0])
 
 	//Next is valid if: 
 	//    is valid now and receiver is not ready
+	//    or
 	//    fifo is not empty, receiver is ready and:
-	//          TLAST is not written
+	//          TLAST word is not stored
 	//        or
-	//          TLAST is written and we are not on the last word
+	//          TLAST word is stored and is not on the last word
 	//        or
-	//          TLAST is written, we are on the last word and on a valid
+	//          TLAST word is stored, is on the last word and on a valid
 	//          portion of wstrb
-   `IOB_REG(clk, tvalid_tmp, (tvalid_tmp & ~tready) | (~fifo_empty & tready & (!written_last_word | fifo_level>num_outputs_per_input | last_wstrb[num_outputs_per_input-fifo_level]))) 
-   `IOB_VAR2WIRE(tvalid_tmp, tvalid)
+   `IOB_REG(clk, tvalid_int, (tvalid_int & ~tready) | (~fifo_empty & tready & (!storing_tlast_word | fifo_level>num_outputs_per_input | last_wstrb[num_outputs_per_input-fifo_level]))) 
+   `IOB_VAR2WIRE(tvalid_int, tvalid)
+
+	//Next is tlast if: 
+	//    is tlast now and receiver is not ready
+	//    or
+	//    fifo is not empty, receiver is ready and TLAST word is stored, is on the last word and:
+	//        next portion of wstrb is zero (meaning this is the last portion of wstrb) (can only happen if fifo_level>1)
+	//        or
+	//        fifo_level is 1 (only reaches this value when wstrb is all ones)
+   `IOB_REG(clk, tlast_int, (tlast_int & ~tready) | (~fifo_empty & tready & fifo_level<=num_outputs_per_input & (fifo_level>1?~last_wstrb[num_outputs_per_input-fifo_level+1]:fifo_level==1))) 
+	// TLAST active only while data is valid
+   `IOB_VAR2WIRE(tlast_int & tvalid_int, tlast)
 
 	//Convert ext_mem_w_en signal to byte enable signal
 	localparam num_bytes_per_output = TDATA_W/8;
